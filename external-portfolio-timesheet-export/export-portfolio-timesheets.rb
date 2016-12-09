f913d58b9e1b3051955bf0900d2604e926c27646
Ananth Rajaraman <Ananth.Rajaraman@ca.com>
2016-12-08 16:06:39 -0800

    bug fixes

--- external-portfolio-timesheet-export/export-portfolio-timesheets.rb
+++ external-portfolio-timesheet-export/export-portfolio-timesheets.rb
@@ -50,24 +50,24 @@ def check_usage()
     puts $!.to_s                                                           # Friendly output when parsing fails
     puts optparse                                                          #
     exit                                                                   #
-  end      
+  end
 
   if @options.auth_file.nil? || !FileTest.exist?(@options.auth_file)
-    puts 
+    puts
     puts "Authorization file #{@options.auth_file} does not exist"
-    puts 
+    puts
     exit 1
   end
 
   if (@options.export_mode == "email" || @options.export_mode == "pv"  )
     if @options.template_file.nil? || !FileTest.exist?(@options.template_file)
-      puts 
+      puts
       puts "Email Template file #{@options.template_file} does not exist"
-      puts 
+      puts
       exit 1
     end
-  end  
-  
+  end
+
   require "./#{@options.auth_file}"
 end
 
@@ -77,7 +77,7 @@ def connect_to_rally
     :vendor => "Rally Technical Services",
     :version=> "0.1"
   )
-  
+
   config = {
     :base_url   => "#{$rally_server}/slm",
     :api_key    => $rally_api_key,
@@ -95,29 +95,29 @@ def get_pi_types
   query.fetch = true
   query.query_string = "( Ordinal != -1 )"
   query.order = "Ordinal"
-  
+
   results = @rally.find(query)
-  
+
   types = []
   results.each do |result|
     result.read
     types.push(result)
   end
-  
+
   return types
 end
 
 
 def get_time_values
   if(@options.end_date.nil?)
-    end_date = Date.today - 1 # yesterday 
+    end_date = Date.today - 1 # yesterday
     end_date.strftime("%F")
   else
     begin
       end_date = Date.parse(@options.end_date)
     rescue ArgumentError
       puts "InvalidArgument: Incorrect end date format. Please enter date as YYYY-MM-DD"
-      puts 
+      puts
       exit 1
     end
   end
@@ -136,7 +136,7 @@ def get_time_values
       start_date = Date.parse(@options.start_date)
     rescue ArgumentError
       puts "InvalidArgument: Incorrect start date format. Please enter date as YYYY-MM-DD"
-      puts 
+      puts
       exit 1
     end
   end
@@ -168,10 +168,10 @@ def add_time_entry_to_time_values(time_values)
   time_values.each do |time_value|
     time_entry_item = time_value["TimeEntryItem"]
     time_entry_item.read
-    puts time_value["ObjectID"].to_s + ":" + time_value["DateVal"].to_s 
+    #puts time_value["ObjectID"].to_s + ":" + time_value["DateVal"].to_s
     time_entry_project = time_entry_item["Project"]
     time_entry_project.read
-    
+
     time_entry_project_owner = time_entry_project["Owner"]
     if !time_entry_project_owner.nil?
       time_entry_project_owner.read
@@ -184,7 +184,7 @@ def add_time_entry_to_time_values(time_values)
       "TimeEntryProjectObject" => time_entry_project
     })
   end
-  
+
   return rows
 end
 
@@ -200,17 +200,17 @@ def add_users_to_time_values(rows)
     end
     row["UserObject"] = user
   end
-  
+
   return rows
 end
 
 def get_type_from_ref(ref)
   ref_array = ref.split('/')
-  
+
   oid = ref_array.pop
   type = ref_array.pop
   above = ref_array.pop
-  
+
   if /portfolio/ =~ above
     return "#{above}/#{type}"
   end
@@ -244,16 +244,16 @@ def get_parents(item, hierarchy=[])
 #  puts "Item: #{item['FormattedID']}"
 #  puts "  Field: #{parent_field}"
   parent = item[parent_field]
-  
+
   if parent.nil?
     return hierarchy
   end
   parent.read
 
   hierarchy.push(parent)
-  
+
   hierarchy = get_parents(parent,hierarchy)
-  
+
   return hierarchy
 end
 
@@ -267,7 +267,7 @@ def add_artifact_to_time_values(rows)
       puts "  Warning: This is likely a project time entry."
       next
     end
-    
+
     begin
       artifact.read
     rescue Exception => ex
@@ -275,12 +275,12 @@ def add_artifact_to_time_values(rows)
       puts "    Possible that this item no longer exists #{artifact._refObjectName}/#{artifact.FormattedID}"
     end
     row["Artifact"] = artifact
-    
-    hierarchy = get_parents(artifact) 
-    row["Hierarchy"] = hierarchy 
+
+    hierarchy = get_parents(artifact)
+    row["Hierarchy"] = hierarchy
     updated_rows.push(row)
   end
-  
+
   return updated_rows
 end
 
@@ -289,23 +289,23 @@ def get_field_value(row, field)
   value = nil
 
   row["Hierarchy"].each do |artifact|
-    if value.nil? 
+    if value.nil?
       value = artifact[field]
     end
   end
   return value
 end
-   
+
 def get_so_field_value(row, field)
   value = nil
   if row["TimeEntryProjectObject"]["c_KMDTimeregistrationIntegration"] == "Yes with suboperation substitution"
     value = row["UserObject"]["c_DefaultSAPSubOperation"]
   else
     row["Hierarchy"].each do |artifact|
-      if value.nil? 
+      if value.nil?
         value = artifact[field]
       end
-    end    
+    end
   end
 
   return value
@@ -315,18 +315,18 @@ end
 def get_type_field_value(record, pi_type, field)
   found_item = nil
   type_path = pi_type['TypePath'].downcase
-  
+
   record["Hierarchy"].each do |artifact|
     type = artifact["_type"] || get_type_from_ref(artifact["_ref"])
     if type.downcase == type_path
       found_item = artifact
     end
   end
-  if found_item.nil? 
+  if found_item.nil?
     return ""
   end
-  return found_item[field]    
-      
+  return found_item[field]
+
 end
 
 def convert_to_output_array(rows,pi_types)
@@ -429,14 +429,14 @@ def get_columns()
           }];
 end
 
-def get_header(columns)
+def get_header(columns,error)
   heads = []
   columns.each do |column|
     if column['dataIndex'] != "ProjectOwnerEmail"
       heads.push(escape_text_for_csv(column['text']))
     end
   end
-  if (@options.export_mode == "pv")
+  if (error)
       heads.push('Reason')
   end
   return heads
@@ -445,7 +445,7 @@ end
 
 def get_csv_from_keys(rows,error)
   columns = get_columns()
-  csv_array = [get_header(columns)]
+  csv_array = [get_header(columns,error)]
   rows.each do |row|
     reason = is_valid_by_keys(row)
     if !error ||  reason != "valid"
@@ -460,16 +460,16 @@ def get_csv_from_keys(rows,error)
       csv_array.push(row_csv_array)
     end
   end
-  
+
   return csv_array
 end
 
 def is_valid_by_keys(row)
   valid = "valid"
-  if (row['c_SAPProject'] != nil) && (row['c_SAPNetwork'] != nil) && (row['c_SAPOperation'] != nil) #&& (row['c_KMDEmployeeID'] != nil)
+  if (row['c_SAPProject'] != nil) && (row['c_SAPNetwork'] != nil) && (row['c_SAPOperation'] != nil) && (row['c_KMDEmployeeID'] != nil)
     if !validate_keys(row)
       valid = $reason_1
-    end 
+    end
   else
     valid = $reason_2
   end
@@ -478,14 +478,21 @@ def is_valid_by_keys(row)
 end
 
 def validate_keys(row)
-  return  (@network.include? row['c_SAPNetwork']) && (@project.include? row['c_SAPProject']) && (@operation.include? row['c_SAPOperation'])
+  if(row['c_SAPSubOperation'] != nil)
+    all_sap_keys = row['c_SAPProject'] + row['c_SAPNetwork'].to_s + row['c_SAPOperation'].to_s + row['c_SAPSubOperation'].to_s
+    return  @sap_keys_all.include? all_sap_keys
+  else
+    no_so_sap_keys = row['c_SAPProject'] + row['c_SAPNetwork'].to_s + row['c_SAPOperation'].to_s
+    return  @sap_keys_no_so.include? no_so_sap_keys
+  end
 end
 
 def get_csv(rows,error)
   columns = get_columns()
-  csv_array = [get_header(columns)]
+  csv_array = [get_header(columns,error)]
   rows.each do |row|
-    if !error || !is_valid(row)
+    reason = is_valid_by_keys(row)
+    if !error ||  reason != "valid"
       row_csv_array = []
       columns.each do |column|
         field = column['dataIndex']
@@ -493,23 +500,24 @@ def get_csv(rows,error)
           row_csv_array.push(escape_text_for_csv(row[field]))
         end
       end
+      if(error)
+        row_csv_array.push(reason)
+      end
       csv_array.push(row_csv_array)
     end
   end
- 
   return csv_array
 end
 
 
 #create a map of csv_array for each owner email address
-# csv_array = {"1st@email.address" => [[get_header(columns)],[row_values]],"2nd@email.address" => [[get_header(columns)],[row_values]]}
 def get_split_csv(rows,error)
   columns = get_columns()
-  csv_array = {} #[get_header(columns)]
+  csv_array = {} 
   rows.each do |row|
     if !error || !is_valid(row)
       if csv_array[row["ProjectOwnerEmail"]].nil?
-        csv_array[row["ProjectOwnerEmail"]] = [get_header(columns)]
+        csv_array[row["ProjectOwnerEmail"]] = [get_header(columns,error)]
       end
       row_csv_array = []
       columns.each do |column|
@@ -526,12 +534,12 @@ end
 
 def get_split_csv_from_keys(rows,error)
   columns = get_columns()
-  csv_array = {} #[get_header(columns)]
+  csv_array = {} 
   rows.each do |row|
     reason = is_valid_by_keys(row)
     if !error || reason != "valid"
       if csv_array[row["ProjectOwnerEmail"]].nil?
-        csv_array[row["ProjectOwnerEmail"]] = [get_header(columns)]
+        csv_array[row["ProjectOwnerEmail"]] = [get_header(columns,error)]
       end
       row_csv_array = []
       columns.each do |column|
@@ -551,8 +559,8 @@ end
 def export_csv(rows)
   csv = get_csv(rows,false)
   filename = "export_#{@time_now}.csv"
-  
-  puts "Writing to #{filename}"  
+
+  puts "Writing to #{filename}"
   CSV.open("#{filename}", "wb") do |csv_file|
     csv.each do |csv_row|
       csv_file << csv_row
@@ -572,7 +580,7 @@ def errors_csv(rows)
       csv_file << csv_row
     end
   end
-  
+
 
   if (@options.export_mode == "email" || @options.export_mode == "pv")
     send_email(filename, rows,$to_address, @time_now)
@@ -582,14 +590,14 @@ end
 
 def sap_headers_xml(rows)
   filename = "E1CATS_INSERT_#{@time_now}.xml"
-  
-  puts "Writing to #{filename}"  
+
+  puts "Writing to #{filename}"
 
 
   builder = Nokogiri::XML::Builder.new do |xml|
     xml.E1CATS_INSERT {
       rows.each do |row|
-        if is_valid(row)  
+        if is_valid(row)
           xml.Datarow {
             xml.GUID row['ObjectID']
             xml.PROFILE row['c_KMDEmployeeID']
@@ -607,13 +615,13 @@ end
 
 def sap_data_xml(rows)
   filename = "E1BPCATS1_#{@time_now}.xml"
-  
-  puts "Writing to #{filename}"  
+
+  puts "Writing to #{filename}"
 
   builder = Nokogiri::XML::Builder.new do |xml|
     xml.E1CATS_INSERT {
       rows.each do |row|
-        if is_valid(row)   
+        if is_valid(row)
           xml.Datarow {
             xml.GUID row['ObjectID']
             xml.WORKDATE row['Date']
@@ -639,13 +647,13 @@ end
 
 def sap_trailer_xml(rows)
   filename = "E1BPCATS8_#{@time_now}.xml"
-  
-  puts "Writing to #{filename}"  
+
+  puts "Writing to #{filename}"
 
   builder = Nokogiri::XML::Builder.new do |xml|
     xml.E1CATS_INSERT {
       rows.each do |row|
-        if is_valid(row)     
+        if is_valid(row)
           xml.Datarow {
             xml.GUID row['ObjectID']
             xml.ROW "1"
@@ -661,30 +669,12 @@ def sap_trailer_xml(rows)
 end
 
 def is_valid(row)
-  if (row['c_SAPNetwork'] != nil) && (row['c_SAPOperation'] != nil) && (row['c_KMDEmployeeID'] != nil)
+  if (row['c_SAPProject'] != nil) && (row['c_SAPNetwork'] != nil) && (row['c_SAPOperation'] != nil) && (row['c_KMDEmployeeID'] != nil)
     return true
   else
     return false
   end
 end
-
-
-def is_valid_key(row)
-  valid = "valid"
-  if (row['c_SAPProject'] != nil) && (row['c_SAPNetwork'] != nil) && (row['c_SAPOperation'] != nil) && (row['c_KMDEmployeeID'] != nil)
-    if !validate_keys(row)
-      valid = "Does not match SAP Keys file"
-    end 
-  else
-    valid = "One or more of the SAP Key is Missing"
-  end
-  return valid
-end
-
-def validate_keys(row)
-  return  (@network.include? row['c_SAPNetwork'].to_s) && (@project.include? row['c_SAPProject']) && (@operation.include? row['c_SAPOperation'].to_s)
-end
-
 
 def date_of_prev(day)
   date  = Date.parse(day)
@@ -722,10 +712,10 @@ def send_email(filename,rows,to_address,time_now)
       html_part do
         content_type 'text/html; charset=UTF-8'
         body  template_file
-      end      
+      end
     end
 
-    mail.deliver!  
+    mail.deliver!
   end
 
 end
@@ -733,26 +723,28 @@ end
 def load_keys_from_csv
 
   if @options.keys_file.nil? || !FileTest.exist?(@options.keys_file)
-    puts 
+    puts
     puts "SAP Keys file #{@options.keys_file} does not exist"
-    puts 
+    puts
     exit 1
   end
 
   file = @options.keys_file #"SAP-Keys.csv"
-  
-  @project = []
-  @network = []
-  @operation = []
-  @sub_operation = []
 
+  # @project = []
+  # @network = []
+  # @operation = []
+  # @sub_operation = []
+  @sap_keys_all = []
+  @sap_keys_no_so = []
   CSV.foreach(file, :col_sep => ";", :return_headers => false) do |row|
-    @project << row[0]
-    @network << row[4]
-    @operation << row[6]
-    @sub_operation << row[8]
+    # @project << row[0]
+    # @network << row[4]
+    # @operation << row[6]
+    # @sub_operation << row[8]
+    @sap_keys_all << (row[0].nil? ? "" : row[0]) + (row[4].nil? ? "" : row[4]) + (row[6].nil? ? "" : row[6]) + (row[8].nil? ? "" : row[8])
+    @sap_keys_no_so << (row[0].nil? ? "" : row[0]) + (row[4].nil? ? "" : row[4]) + (row[6].nil? ? "" : row[6])
   end
-
 end
 
 ## - start here -
